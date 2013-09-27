27ed43aee4b105d391ea94a3ab140319fe3f8a91
Ankit Singh <ankitsingh.05@gmail.com>
2013-09-27 19:40:26 +0200

    initial commit

---
+++ src/duff_utils.cpp
@@ -0,0 +1,10 @@
+/* ----------------------------------------------------------------------------------
+* Various Utils like hash functions, type conversions etc. will contain in this 
+* class.
+*
+* @Author Ankit Singh (iankits) 
+*
+*
+------------------------------------------------------------------------------------*/
+
+#include "duff_utils.h"
---
+++ src/duff_utils.h
@@ -0,0 +1,17 @@
+/*-------------------------------------------------------------------
+Duff Util header class
+
+@Author Ankit Singh
+@Copyright 2013
+--------------------------------------------------------------------*/
+
+#ifndef __DUFF_UTIL_H_INCLUDED__
+#define __DUFF_UTIL_H_INCLUDED__
+
+class duff_utils {
+	public:
+		int get_hash(); // TODO: get type to pass function
+
+}
+
+#endif __DUFF_UTIL_H_INCLUDED__
